e2449a6ffea93d04c7d6f45bb817249713f0336e
Ryan Santos <ryansantos.gp3r@gmail.com>
2026-08-16 00:29:52 -0300

    estudo de 'herança'.

---
+++ src/portfolio/sainttdev/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,30 @@
+package portfolio.sainttdev.maratonajava.javacore.Hheranca.test;
+
+import portfolio.sainttdev.maratonajava.javacore.Hheranca.dominio.Endereco;
+import portfolio.sainttdev.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import portfolio.sainttdev.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Doido do Centro");
+        pessoa.setCpf("123.456.789-10");
+        pessoa.setEndereco(endereco);
+
+        pessoa.imprime();
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Mark Zukemberg");
+        funcionario.setCpf("55555");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(95.628);
+
+        System.out.println("-----------");
+
+        funcionario.imprime();
+    }
+}
